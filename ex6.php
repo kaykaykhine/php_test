fe495529051342e4c5477fcea0462a35d3b4c40f
aceplu040-PC\user <ayephupoepoe@gmail.com>
2017-08-24 14:45:55 +0630

    Update

--- ex6.php
+++ ex6.php
@@ -1,12 +1,4 @@
 <?php
-//AS by value
-echo "Ascending order sort by value<br>";
-$result1=array("Sophia"=>"31","Jacob"=>"41","William"=>"39","Ramesh"=>"40"); 
-asort($result1);
-foreach($result1 as $a=>$a_value)
-{
-echo "AS value of ".$a." is : ".$a_value."<br>";
-}
 //AS by key
 echo "Ascending order sort by Key<br>";
 $result2=array("Sophia"=>"31","Jacob"=>"41","William"=>"39","Ramesh"=>"40"); 
