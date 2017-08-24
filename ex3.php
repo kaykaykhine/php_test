837bd342d7263ac510473d48873f7dca01256a54
aceplu052\ace <ayeayelin@aceplussolutions.com>
2017-08-24 15:04:46 +0630

    edited form ayeaye

--- ex3.php
+++ ex3.php
@@ -2,7 +2,7 @@
 	 $color = array(4 => 'white', 6 => 'green', 11=> 'red');
 	 echo current($color) . "<br>";
 	 echo next($color) . "<br>";
-	 echo next($color) . "<br>";
+	 echo next($color) ."Testing from ayeaye". "<br>";
 	 echo prev($color) . "<br>";
 	 echo current($color) ."<br>";
 	 echo next($color) . "<br>";
