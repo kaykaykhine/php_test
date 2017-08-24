5255f5f74f40d1f633073cbba3f6bb99a9ce4f7d
ACEPLU041\aceplu041 <hninhninaye@aceplussolutions.com>
2017-08-24 14:46:45 +0630

    testing from hninhnin

--- ex3.php
+++ ex3.php
@@ -2,6 +2,7 @@
 	 $color = array(4 => 'white', 6 => 'green', 11=> 'red');
 	 echo current($color) . "<br>";
 	 echo next($color) . "<br>";
+	 echo "Testing from hninhnin.";
 	 echo next($color) . "<br>";
 	 echo prev($color) . "<br>";
 	 echo current($color) ."<br>";
